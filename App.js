c66980b932a0de0d5c6d17b46968cd8262456f35
Luan Rohde <luanrohde11@gmail.com>
2018-09-27 23:49:53 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import 'config/ReactotronConfig';
+
+const App = () => (
+  <View>
+    <Text>Welcome to React Native!</Text>
+  </View>
+);
+
+export default App;
